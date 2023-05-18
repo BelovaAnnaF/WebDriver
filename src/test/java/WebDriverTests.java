7c0f3f45b2325dcb585eb64e48695f8cae3e7c9b
belova <belova@geobuilder.ru>
2023-05-18 12:48:42 +0300

    testOpenImage

--- src/test/java/WebDriverTests.java
+++ src/test/java/WebDriverTests.java
@@ -4,8 +4,11 @@ import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
@@ -23,6 +26,7 @@ public class WebDriverTests {
 //    Открыть Chrome в headless режиме
         WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");//без этого не работает
         options.addArguments("--headless");
         driver = new ChromeDriver(options);
 //    Перейти на https://duckduckgo.com/
@@ -33,9 +37,7 @@ public class WebDriverTests {
         driver.findElement(By.cssSelector("#search_button_homepage")).click();
 //    Проверить что в поисковой выдаче первый результат Онлайн‑курсы для профессионалов, дистанционное обучение
         driver.findElement(By.cssSelector(".react-results--main>li:first-child")).click();
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
-//        driver.findElement(By.cssSelector("head>[content *= 'Онлайн‑курсы для профессионалов']"));
-        assertEquals("Онлайн‑курсы для профессионалов, дистанционное обучение", driver.getTitle());
+        assertEquals("Онлайн‑курсы для профессионалов, дистанционное обучение современным профессиям", driver.getTitle());
     }
 //2)
     @Test
@@ -43,6 +45,7 @@ public class WebDriverTests {
 //    Открыть Chrome в режиме киоска
         WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
         options.addArguments("--kiosk");
         driver = new ChromeDriver(options);
 //    Перейти на https://demo.w3layouts.com/demos_new/template_demo/03-10-2020/photoflash-liberty-demo_Free/685659620/web/index.html?_ga=2.181802926.889871791.1632394818-2083132868.1632394818
@@ -50,7 +53,8 @@ public class WebDriverTests {
 //    Нажать на любую картинку
         driver.findElement(By.cssSelector(".content-overlay")).click();
 //    Проверить что картинка открылась в модальном окне
-
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+        wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.cssSelector("div.pp_overlay"))));
     }
 
 //3)
@@ -60,6 +64,7 @@ public class WebDriverTests {
 //    Открыть Chrome в режиме полного экрана
         WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
         options.addArguments("--start-fullscreen");
         driver = new ChromeDriver(options);
 //    Перейти на https://otus.ru
